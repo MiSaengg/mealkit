950348ceb29c8d3e266d9a468b14419c519b5dd9
David Cho <gcho13@my.bcit.ca>
2023-09-19 20:50:32 -0700

    [Add] API route for find markets by query

--- src/main/java/com/example/mealkit/repository/MarketRepository.java
+++ src/main/java/com/example/mealkit/repository/MarketRepository.java
@@ -2,8 +2,18 @@ package com.example.mealkit.repository;
 
 import com.example.mealkit.model.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-
 public interface MarketRepository extends JpaRepository<Market, Long> {
+    @Query("SELECT m FROM Market m WHERE " +
+            "(:name IS NULL OR m.name = :name) AND " +
+            "(:location IS NULL OR m.location = :location) AND " +
+            "(:ratingURL IS NULL OR m.ratingURL = :ratingURL)")
+    List<Market> findByQuery(@Param("name") String name,
+                             @Param("location") String location,
+                             @Param("ratingURL") String ratingURL);
 }
+
+
