25713a9c98ebd5d1605ca2e32f971ce51c7abe94
Jasper Oh <ohyj0906@gmail.com>
2023-11-22 16:24:28 -0800

    [Fix, GroundLevel] Start recharts

--- mealkit-frontend/app/sellerWeb/layout.tsx
+++ mealkit-frontend/app/sellerWeb/layout.tsx
@@ -10,9 +10,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       {/* 여기에 헤더, 푸터 등 공통 요소를 추가 */}
-      {children}
       <SellerHeader></SellerHeader>
-      <Sidebar></Sidebar>
+        <Sidebar onSectionSelect={null}></Sidebar>
       <SellerFooter></SellerFooter>
       
     </div>
