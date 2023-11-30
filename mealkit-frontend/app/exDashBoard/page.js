f10dcc10f02cf08ecbf36f7cac05e90f911a9038
Jasper Oh <ohyj0906@gmail.com>
2023-11-29 16:24:03 -0800

    [Add] Componenets seperate with grid-tailwind & Alex will cover the rechart parts

--- mealkit-frontend/app/exDashBoard/page.js
+++ mealkit-frontend/app/exDashBoard/page.js
@@ -1,9 +1,28 @@
+"use client"
 import React from 'react';
+import WeeklyOrderNumber from '../../components/exDashBoardComponents/WeeklyOrderNumber';
+import WeeklyOrderRequest from '../../components/exDashBoardComponents/WeeklyOrderRequest';
+import WeeklyOutcome from '../../components/exDashBoardComponents/WeeklyOutcome';
+import WeeklyReviewStatus from '../../components/exDashBoardComponents/WeeklyReviewStatus';
+
 
 function page(props) {
   return (
-    <div>
-      Hello World
+    <div className='bg-amber-300'>
+      <div className="container grid grid-cols-2 gap-4">
+        <div className='p-4 box-border h-32 p-4 border-4'>
+          <WeeklyOrderNumber/>
+        </div>
+        <div className='p-4 box-border h-32 p-4 border-4'>
+          <WeeklyOutcome/>
+        </div>
+        <div className='p-4 box-border h-32 p-4 border-4'>
+          <WeeklyOrderRequest/>
+        </div>
+        <div className='p-4 box-border h-32 p-4 border-4'>
+          <WeeklyReviewStatus/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ mealkit-frontend/components/exDashBoardComponents/WeeklyOrderNumber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WeeklyOrderNumber(props) {
+  return (
+    <div>
+      This is WeeklyOrderNumber
+    </div>
+  );
+}
+
+export default WeeklyOrderNumber;
