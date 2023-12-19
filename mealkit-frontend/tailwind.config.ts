c06d4ec18202d04c038426d9f7dd6fefdeb3b46c
Jasper Oh <ohyj0906@gmail.com>
2023-12-18 16:32:01 -0800

    [Add] Change folder strcuture and finish the sidebar implementation

---
+++ mealkit-frontend/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [    
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config;
